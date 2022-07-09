c69ca981134facf510689585ad713b8d5954e25c
21107004 <105075787+21107004@users.noreply.github.com>
2022-07-09 12:55:01 +0530

    Add files via upload

---
+++ Assignment6 python question 6th.py
@@ -0,0 +1,12 @@
+
+# In[ ]:
+
+
+def student_data(student_name , student_branch, student_id):
+    print("student name: ",student_name)
+    print("student branch: ",student_branch)
+    print("student id: ",student_id)
+
+student_data("Aayush","Civil",21102050)
+
+
